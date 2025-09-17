bd4890d7f792440b30e7818ffc4316f197acb727
Henry Oladeji <henryoladeji2007@gmail.com>
2025-09-17 19:41:28 +0100

    updated settings for database

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -90,7 +90,34 @@
  * ];
  * @endcode
  */
-$databases = [];
+// $databases = [
+//   'default' => [
+//     'default' => [
+//       'database' => getenv('DB_NAME'),
+//       'username' => getenv('DB_USER'),
+//       'password' => getenv('DB_PASSWORD'),
+//       'host' => getenv('DB_HOST'),
+//       'port' => getenv('DB_PORT'),
+//       'driver' => 'mysql',
+//       'prefix' => '',
+//       'collation' => 'utf8mb4_general_ci',
+//       'sslmode' => 'required',
+//     ],
+//   ],
+// ];
+
+$databases['default']['default'] = [
+  'database' => getenv('DB_NAME'),
+  'username' => getenv('DB_USER'),
+  'password' => getenv('DB_PASSWORD'),
+  'host' => getenv('DB_HOST'),
+  'port' => getenv('DB_PORT'),
+  'driver' => 'mysql', // <-- MySQL here
+  'prefix' => '',
+  'collation' => 'utf8mb4_general_ci',
+  'sslmode' => 'required',
+];
+
 
 /**
  * Customizing database settings.
