46745b1101bd1f8b4729735188075af85e99d167
Henry Oladeji <henryoladeji2007@gmail.com>
2025-09-29 15:04:35 +0100

    added japachronciles multisite

---
+++ web/sites/japachronicles.xyz/settings.php
@@ -0,0 +1,51 @@
+<?php
+
+
+$databases['default']['default'] = [
+    'database' => $_ENV['JAPACHRONICLES_MYSQL_DATABASE'],
+    'driver' => 'mysql',
+    'host' => $_ENV['JAPACHRONICLES_MYSQL_HOSTNAME'],
+    'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+    'password' => $_ENV['JAPACHRONICLES_MYSQL_PASSWORD'],
+    'port' => $_ENV['JAPACHRONICLES_MYSQL_PORT'],
+    'prefix' => '',
+    'username' => $_ENV['JAPACHRONICLES_MYSQL_USER'],
+    'pdo' => [
+        PDO::MYSQL_ATTR_SSL_CA => '/var/www/japachronicles.crt',
+    ],
+    'isolation_level' => 'READ COMMITTED',
+];
+
+
+$settings['hash_salt'] = $_ENV['JAPACHRONICLES_HASH_SALT'];
+
+
+
+if (empty($settings['config_sync_directory'])) {
+    $settings['config_sync_directory'] = '../config/sync/japachronicles.xyz';
+}
+
+
+$settings['skip_permissions_hardening'] = TRUE;
+$settings['trusted_host_patterns'] = [
+    '^japachronicles\.xyz$',
+    '^www\.japachronicles\.xyz$',
+];
+
+
+$settings['file_scan_ignore_directories'] = [
+    'node_modules',
+    'bower_components',
+];
+
+
+$settings['entity_update_batch_size'] = 50;
+
+
+$settings['entity_update_backup'] = TRUE;
+
+
+$settings['state_cache'] = TRUE;
+
+
+$settings['migrate_node_migrate_type_classic'] = FALSE;
---
+++ web/sites/sites.php
@@ -0,0 +1,6 @@
+<?php
+
+$sites["www.japachronicles.xyz"] = "japachronicles.xyz";
+$sites["penzest.dev"] = "default";
+
+$sites["www.penzest.com"] = "penzest.com";
