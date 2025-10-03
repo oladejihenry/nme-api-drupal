f0ac3aaeb66b1c05c81d80edddd8ae9f447b8a0f
Henry Oladeji <henryoladeji2007@gmail.com>
2025-10-03 16:43:43 +0100

    added file permission solution

---
+++ permission.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Ensure all required directories exist
+DIRS=(
+  web/sites/default/files
+  web/sites/default/files/private
+  web/sites/default/files/tmp
+  web/sites/default/files/sync
+)
+
+for dir in "${DIRS[@]}"; do
+  mkdir -p "$dir"
+done
+
+# Set permissions recursively
+chmod -R 775 web/sites/default/files
+
+# Clear Drupal caches
+./vendor/bin/drush cr
